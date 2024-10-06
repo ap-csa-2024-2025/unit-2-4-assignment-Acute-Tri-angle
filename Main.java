dc715af33e7613a107e405d1a2d097281292977e
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-10-05 19:11:32 -0700

    bugfixes w/ input commands

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Main
 {
   public static void main(String[] args)
   {
     // write your code here
-    System.out.println(lastFirstN("behemoth", "emaculate",4));
-    stringManip("tuvalu", "australia");
+    Scanner sc = new Scanner (System.in);
+    System.out.println("First String?");
+    String s1 = sc.nextLine();
+    System.out.println("Second String?");
+    String s2 = sc.nextLine();
+    System.out.println("First/Last n ?");
+    int n = sc.nextInt();
+    System.out.println(lastFirstN(s1,s2,n));
+    sc.nextLine();
+
+    System.out.println("First String?");
+    s1 = sc.nextLine();
+    System.out.println("Second String?");
+    s2 = sc.nextLine();
+    stringManip(s1, s2);
   }
   public static String lastFirstN(String s1, String s2, int n){
-    return s1.substring(s1.length() - n, s1.length()) + s2.substring(0, s2.length() - (n+1));
+    return s1.substring(s1.length() - n, s1.length()) + s2.substring(40, n);
   }
   public static void stringManip(String x, String y){
     String a = x.toUpperCase();
